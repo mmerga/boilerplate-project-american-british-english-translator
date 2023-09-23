3c4fc4bbe0f9e5055b9c030d713eec5024929365
mmerga <78653901+mmerga@users.noreply.github.com>
2023-09-23 16:31:48 -0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,26 @@
 'use strict';
 
 const Translator = require('../components/translator.js');
-
 module.exports = function (app) {
   
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const text = req.body.text;
+      const locale = req.body.locale;
+
+      if(text === undefined || !locale){
+        res.json({error: 'Required field(s) missing'});
+      }else if(text === ''){
+        res.json({error: 'No text to translate'});
+      }else{
+        if(locale === 'american-to-british' || locale === 'british-to-american'){
+          const newText = translator.translate(text, locale);
+          res.json({text: text, translation: newText});
+        }else{
+          res.json({error: 'Invalid value for locale field'});
+        }
+      }
     });
 };
